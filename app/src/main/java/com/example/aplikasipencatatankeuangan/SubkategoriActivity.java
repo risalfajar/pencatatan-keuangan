3871d3c92e2b7a76a0e7337ed758f5b1d748d6dd
Risal <risalfajar@gmail.com>
2019-09-10 00:07:32 +0700

    First Commit

---
+++ app/src/main/java/com/example/aplikasipencatatankeuangan/SubkategoriActivity.java
@@ -0,0 +1,13 @@
+package com.example.aplikasipencatatankeuangan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubkategoriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subkategori);
+    }
+}
